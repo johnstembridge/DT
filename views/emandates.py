e0c1e36aec788a88e54e54d9b4ddda6cdf5f3f67
johnstembridge <john.stembridge@gmail.com>
2021-03-04 15:06:55 +0000

    Interface to Emandates - use get_data() on callback

--- views/emandates.py
+++ views/emandates.py
@@ -6,9 +6,9 @@ from front_end.emandates import Emandates
 
 @app.route('/emandates/confirm', methods=['GET'])
 def emandates_confirm():
+    app.logger.info(request.get_data())
     app.logger.info(request)
     app.logger.info(request.args)
-    app.logger.info(request.form)
     app.logger.info("confirmed")
     return Emandates.emandates_confirm(request.args)
 
