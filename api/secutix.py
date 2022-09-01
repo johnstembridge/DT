64279565030cc9cc836b59bda0d6f1752cae7645
johnstembridge <john.stembridge@gmail.com>
2022-09-01 14:47:05 +0100

    temp fix for requests

--- api/secutix.py
+++ api/secutix.py
@@ -1,14 +1,20 @@
 import jwt
-import requests
+#import requests
 from datetime import datetime, timedelta, timezone
 from flask import abort
+
+
+class requests:
+
+    @staticmethod
+    def post(endpoint, json, headers):
+        return None
 
 
 class Secutix:
 
     service_url = "https://afcw.pp-ws.secutix.com/tnco/backend-apis/"
 
-    @staticmethod
     def forge_jwt():
         iat = datetime.now(tz=timezone.utc)
         payload = {
